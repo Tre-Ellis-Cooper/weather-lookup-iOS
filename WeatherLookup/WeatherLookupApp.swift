cd303884177987fb17a406534f1f9215a8ce9bb7
Tre Ellis Cooper <treellis.cooper@publicissapient.com>
2021-02-21 16:38:37 -0600

    Project initialization.

---
+++ WeatherLookup/WeatherLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherLookupApp.swift
+//  WeatherLookup
+//
+//  Created by Tre Cooper on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
